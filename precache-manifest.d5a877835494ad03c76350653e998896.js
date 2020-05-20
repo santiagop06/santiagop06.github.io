18114135acddd18f52ca695d25b88585ed1899f5
santiagop06 <santiagopuentes8@gmail.com>
2020-05-20 16:48:20 -0500

    Updates

---
+++ precache-manifest.d5a877835494ad03c76350653e998896.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffc57575d934430b51fdc668658f61c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "550e028b9c784bcaef3d",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ebd715c0375a59eed3d9",
+    "url": "/static/css/main.d9ab7f10.chunk.css"
+  },
+  {
+    "revision": "550e028b9c784bcaef3d",
+    "url": "/static/js/2.c8c54797.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.c8c54797.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebd715c0375a59eed3d9",
+    "url": "/static/js/main.28ab7287.chunk.js"
+  },
+  {
+    "revision": "d875b6f2ec20d12f3323",
+    "url": "/static/js/runtime-main.6a1508d6.js"
+  },
+  {
+    "revision": "f40cc49dddf18c087a41e8c0c01154ed",
+    "url": "/static/media/background.f40cc49d.PNG"
+  },
+  {
+    "revision": "1c58860633aa3b4134988eff4f080011",
+    "url": "/static/media/faceapp.1c588606.PNG"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "412715e001e7e6fde37773e8bfbbfa7a",
+    "url": "/static/media/robofriends.412715e0.PNG"
+  }
+]);
